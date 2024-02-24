41e6d439389b3aa6629228172a2284dda6644371
Gustavo Alencar Silva <gustavoalens@gmail.com>
2024-02-24 18:20:21 -0300

    Prepare Smile recognizer

--- FaceLivenessFramework/Sources/FaceLivenessFramework/Validations/Expression/SmileRecognizer.swift
+++ FaceLivenessFramework/Sources/FaceLivenessFramework/Validations/Expression/SmileRecognizer.swift
@@ -1,10 +1,16 @@
 import ARKit
+import Combine
 
 final class SmileRecognizer: FaceExpressionRecognizerProtocol {
     let shapeType: Set<ARFaceAnchor.BlendShapeLocation> = [.mouthSmileLeft, .mouthSmileRight]
+    private var subscriptions: Set<AnyCancellable> = Set()
+    private let controlPublisher: PassthroughSubject<FaceAnchorChange, Never> = PassthroughSubject<FaceAnchorChange, Never>()
+    
+    private let queue: DispatchQueue = .init(label: String(describing: SmileRecognizer.self))
+    @Published private var result: Bool = false
     
     func start() {
-        // TODO: implement
+        setupPublisher()
     }
     
     func stop() {
@@ -12,11 +18,48 @@ final class SmileRecognizer: FaceExpressionRecognizerProtocol {
     }
     
     func didChanged(variations: [ARFaceAnchor.BlendShapeLocation : NSNumber?]) {
-        // TODO: implement
+        controlPublisher.send(variations)
     }
     
     func recognized() async -> Bool {
-        // TODO: implement
+        start()
+        let result = (try? await $result.async()) ?? false
+        stop()
+        return result
+    }
+    
+    // MARK: - Methods
+    private func setupPublisher() {
+        controlPublisher
+            .collect(.byTime(DispatchQueue.main, .seconds(1)))
+            .receive(on: queue)
+            .reduce([ARFaceAnchor.BlendShapeLocation: [NSNumber]](), { partial, changes in
+                var new = partial
+                changes.forEach { changes in
+                    changes.forEach { dictChange in
+                        new.add(onKey: dictChange.key, value: dictChange.value)
+                    }
+                }
+                return new
+            })
+            .sink { [weak self] variationsByShape in
+                self?.checkVariation(variationsByShape: variationsByShape)
+            }
+            .store(in: &subscriptions)
+    }
+    
+    private func checkVariation(variationsByShape: [ARFaceAnchor.BlendShapeLocation: [NSNumber]]) {
+        let didBlink = shapeType.allSatisfy { type in
+            guard let variation = variationsByShape[type] else { return false } // TODO: Send error result?
+            return isSmile(variation: variation)
+        }
+        if didBlink {
+            result = true
+        }
+    }
+    
+    private func isSmile(variation: [NSNumber]) -> Bool {
         return false
     }
 }
+
